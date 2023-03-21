7863f5b4dd2a359b3d310afffdcaa4e76d71b5bd
Dario Flavio Scipioni <darioflavio98@outlook.it>
2023-03-21 11:54:46 +0100

    Caricamento progetto iniziale Spring Boot

---
+++ be/narpa/src/main/java/it/rotechnology/narpa/NarpaApplication.java
@@ -0,0 +1,13 @@
+package it.rotechnology.narpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarpaApplication.class, args);
+	}
+
+}
---
+++ be/narpa/src/test/java/it/rotechnology/narpa/NarpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.rotechnology.narpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
